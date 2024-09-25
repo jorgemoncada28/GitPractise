488f2a682acde3f43fac68490ffef589e2f49b19
jorgemoncada28 <jj_ark_21@hotmail.com>
2024-09-25 03:07:17 -0500

    updated cart page

--- src/test/java/Pages/CartPage.java
+++ src/test/java/Pages/CartPage.java
@@ -3,5 +3,7 @@ package Pages;
 public class CartPage {
     public static void main(String[] args) {
         System.out.println("This is my cart page");
+
+        System.out.println("cart page feature");
     }
 }
